d3e5e88f949fed3f2ce81199de4d4fc285fa06e7
Justin <j.hebenstreit25@gmail.com>
2024-09-05 19:01:34 -0500

    added some more CSS and files

--- src/assets/images/images.ts
+++ src/assets/images/images.ts
@@ -1,7 +1,9 @@
 import Asphalt from './asphalt-legends-unite-non-react.png';
 import CodingNotesV2 from './coding-notes-react.png';
+import StarWars from './star-wars-site.png';
 
 export const Images = {
     Asphalt,
     CodingNotesV2,
+    StarWars,
 };
--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -4,18 +4,27 @@ import Data from "../components/PortfolioData";
 export default function Portfolio() {
     return (
         <>
-         
+
             <h1 className='pageHeader'>Portfolio</h1>
-            {Data.map((project, index)=>(
-                <Cards
-                key={project.name + index}
-                name={project.name}
-                description={project.description}
-                image={project.image}
-                gitHubRepo={project.gitHubRepo}
-                deployment={project.deployment}
-                />
-            ))}
+
+            <div className="card-container">
+                <div className="row">
+
+                    {Data.map((project, index) => (
+                        <Cards
+                            key={project.name + index}
+                            name={project.name}
+                            description={project.description}
+                            image={project.image}
+                            gitHubRepo={project.gitHubRepo}
+                            deployment={project.deployment}
+
+                        />
+                    ))}
+
+                    </div>
+                </div>
+            {/* </div> */}
 
 
         </>
