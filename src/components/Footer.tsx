ca37fb80c2256ebb4dad5f927fc5f8fa876f84a0
Justin <j.hebenstreit25@gmail.com>
2024-09-05 21:34:21 -0500

    netlify test 3

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,16 +1,16 @@
-import ReactDOM from 'react-dom'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { all } from '@awesome/kit-KIT_CODE/icons'
+// import ReactDOM from 'react-dom'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { library } from '@fortawesome/fontawesome-svg-core'
+// import { all } from '@awesome/kit-KIT_CODE/icons'
 
 import '../css/Footer.css'
 
-library.add(...all)
+// library.add(...all)
 
-const element = <FontAwesomeIcon icon="{['kit', 'my-icon']}" />
+// const element = <FontAwesomeIcon icon="{['kit', 'my-icon']}" />
 // This will cause this Typescript error: `Type is not assignable to type IconProp`
 
-ReactDOM.render(element, document.body)
+// ReactDOM.render(element, document.body)
 
 export default function Footer() {
     return (
@@ -21,7 +21,7 @@ export default function Footer() {
 
             <p>Created by Justin Hebenstreit</p>
             <div>
-            <FontAwesomeIcon icon="fa-brands fa-github" />
+            {/* <FontAwesomeIcon icon="fa-brands fa-github" /> */}
             <a href=""></a>
             </div>
                
