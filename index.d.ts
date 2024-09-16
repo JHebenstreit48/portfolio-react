82aa87beeedb66c9fdac487f0eed3f456c61ec45
Justin <j.hebenstreit25@gmail.com>
2024-09-16 15:31:47 -0500

    fixed and added some code include profile picture

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,5 @@
 declare module '*.png';
+declare module '*.jpg';
 
 type PortfolioItem = {
     title: string;
--- src/assets/images/images.ts
+++ src/assets/images/images.ts
@@ -2,10 +2,12 @@ import Asphalt from "./asphalt-legends-unite-non-react.png";
 import CodingNotesV2 from "./coding-notes-react.png";
 import StarWars from "./star-wars-site.png";
 import CodingNotesV1 from "./coding-notes-v1.png";
+import ProfilePicture from "./profilePicture.jpg";
 
 export const Images = {
   Asphalt,
   CodingNotesV2,
   StarWars,
   CodingNotesV1,
+  ProfilePicture,
 };
