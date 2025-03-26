c7c9cdbd3376325c4ef9c7931610149ed1216e76
Justin <j.hebenstreit25@gmail.com>
2025-03-26 15:33:00 -0500

    updated files for aliases and Tailwind prep

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./src/**/*.{js,ts,jsx,tsx}", // ✅ Leave as-is — don't change to @/
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
