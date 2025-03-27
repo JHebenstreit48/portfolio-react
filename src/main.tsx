b1cd8d292d859c8a4ae6ad56ce750608b71c7216
Justin <j.hebenstreit25@gmail.com>
2025-03-26 22:27:40 -0500

    still working out bugs

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './routes';
-import './index.css';
+import './test.css';
 
 const rootElement = document.getElementById('root');
 
