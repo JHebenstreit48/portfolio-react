2ea7a37d4cd2d12c881d9e909f093ad10b27f2ce
Justin <j.hebenstreit25@gmail.com>
2025-03-26 22:12:36 -0500

    working on Tailwind problems still having issues

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
-import { router } from '@/routes';
+import { router } from './routes';
+import './index.css';
 
 const rootElement = document.getElementById('root');
 
