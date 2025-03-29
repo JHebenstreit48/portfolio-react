c52618d8a2bbc62ec1858e1ffad10f3993e6f216
Justin <j.hebenstreit25@gmail.com>
2025-03-28 20:06:44 -0500

    working on media queries for Skills component of Resume page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -10,14 +10,14 @@ export default function About() {
 
         <img className='profilePicture' src={ProfilePicture} alt='Profile Picture' />
 
-      <div className='about'>
+        <div className='about'>
 
-      <p >
-          I am a Full-Stack Web Developer with a background in photography and music. I have a passion for music, automotive, photography, and gaming. I recently completed a 6 month long Full-Stack Web Development coding bootcamp through Washington University St. Louis in partnership with Edx. I am skilled in HTML, CSS, JavaScript, TypeScript, Node.js, Express.js, PostgreSQL, and React. I am a creative thinker and a team player. I am excited to bring my skills to any team.
-        </p>
+          <p className="bio">
+            I am a Full-Stack Web Developer with a background in photography and music. I have a passion for music, automotive, photography, and gaming. I recently completed a 6 month long Full-Stack Web Development coding bootcamp through Washington University St. Louis in partnership with Edx. I am skilled in HTML, CSS, JavaScript, TypeScript, Node.js, Express.js, PostgreSQL, and React. I am a creative thinker and a team player. I am excited to bring my skills to any team.
+          </p>
 
-      </div>
-        
+        </div>
+
 
       </div>
 
